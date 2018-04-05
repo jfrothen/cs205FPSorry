2a19a1338b4b6728d34da092a827d0f21d8731a2
Joshenberg <jfrothen@uvm.edu>
2018-04-05 17:34:58 -0400

    Starting to fill in functions for the pawn and player files

---
+++ Main/src/Pawn.java
@@ -0,0 +1,72 @@
+public class Pawn {
+	public Object currentNode; //FIX THIS FOR BETTER POINTER TO MAP LOCATION
+	public int distanceFromHome; //acts as relative progress check
+    public boolean isOut;
+	public boolean isSafe;
+	public int colorID;
+	
+	Pawn(int color) {
+		this.colorID = color;
+		locationID = 0;
+		isOut = false;
+		//set currentNode equal to the correct node based on the color
+		isSafe = false;
+		
+		System.out.println("Pawn created, color = " + this.colorID);
+	}
+
+	public Object testMove(int steps, boolean is7) { //FIX THIS AS ABOVE
+
+	    switch(steps) {
+            case 1:
+            case 2: {
+                if (!isOut && !is7) {
+                    return currentNode.nextNode(); //FIX THIS
+                }
+            }
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7: //note: no actual
+            case 8:
+            case 12: {
+                if (!isOut) {
+                    return null;
+                }
+                else if (locationID + )
+                break;
+            }
+
+        }
+
+
+	    //first handle if pawn is still in home
+	    if (!isOut && (draw == 1 || draw == 2 ) {
+	        return firstNode; //MAKE THIS RETURN THE FIRST NODE OFF THE THING
+        }
+        else if (!isOut) {
+	        return null;
+        }
+        //next handle if pawn is too close to home to go anywhere
+        else if (locationID + draw > 65) {
+	        return null;
+        }
+
+        else {
+            //GOES THROUGH THE STEPS OF THE MAP TO DERIVE THE NEW NODE
+            //MUST FIX
+        }
+
+	    return endNode;
+    }
+
+    public int rateMove(int draw) {
+	    int rating = 0;
+
+	    if (locationID + draw)
+
+    }
+	
+	
+}
