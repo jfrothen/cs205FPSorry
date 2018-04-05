e7b0b033885bb490895a34932d7625fbecb7d498
Joshenberg <jfrothen@uvm.edu>
2018-04-05 12:58:19 -0400

    Starting to fill in functions for the pawn and player files

--- Player.java
+++ Player.java
@@ -11,6 +11,26 @@ public class Player {
 		System.out.println("Player created with color =  " + this.color);
 		
 	}
+
+	public List<Object> getMoveList(int draw) {
+		List<Object> moveList = new List();
+
+		switch (draw){
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 8:
+			case 12: {
+				for (Pawn thisPawn : pawnArray) {
+					moveList.push(thisPawn.testMove(draw));
+				}
+				break;
+			}
+			}
+		}
+	}
 	
 }
 
