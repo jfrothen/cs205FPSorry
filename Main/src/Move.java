c41fe79a61682894b8b4d4849962357554f69f9b
Joshenberg <jfrothen@uvm.edu>
2018-04-12 16:23:35 -0400

    Made move a normal class

--- Main/src/Move.java
+++ Main/src/Move.java
@@ -1,13 +1,11 @@
 /*
  Returns a move, containing fields used to rank relative quality of the move in various AI
   */
-private static Class Move {
+public Class Move {
     public boolean bounced; //did this move bounce a player
     public int whomBounced; //which player bounced (winning players are better targets)
     public boolean slid; //if went over a slide
     public boolean gotSafe; //if got
     public boolean gotHome;
     public boolean gotOut;
-
-
 }
