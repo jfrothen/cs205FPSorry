55b85bb61d2ef97f2b90c2526da662304456976f
AMeeker <ameeker@uvm.edu>
2018-04-12 17:00:11 -0400

    Shuffle works

--- Main/src/Deck.java
+++ Main/src/Deck.java
@@ -2,21 +2,34 @@ package Main.src;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Deck {
-    Card[] deck=new Card[45];
+    final int DECKSIZE=45;
+    Card[] deck=new Card[DECKSIZE];
+    ArrayList<Integer> randomizer=new ArrayList<Integer>();
     int TCindex;
 
     public Deck(){
         generateDeck();
-        shuffle();
         TCindex=0;
+        for (int i=0;i<DECKSIZE;i++){
+            randomizer.add(i);
+        }
+        shuffle();
     }
 
 
 
     private void shuffle(){
-
+        Collections.shuffle(randomizer);
+        Card[] tempdeck=new Card[DECKSIZE];
+        for (int j=0;j<DECKSIZE;j++){
+            tempdeck[j]=deck[randomizer.get(j)];
+        }
+        for (int k=0;k<DECKSIZE;k++){
+            deck[k]=tempdeck[k];
+        }
     }
 
     private void generateDeck(){
@@ -30,6 +43,11 @@ public class Deck {
         }
     }
 
+
+    public static void main(String[] args){
+        Deck testdeck=new Deck();
+        System.out.println(Arrays.toString(testdeck.deck));
+    }
 
 }
 
